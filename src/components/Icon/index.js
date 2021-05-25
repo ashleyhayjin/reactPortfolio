124c10971297df0d89200d095464edcf5913d235
Ashley Lee <ashleyhayjinlee@gmail.com>
2021-05-25 16:08:01 -0500

    Jumbotrom

---
+++ src/components/Icon/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Icon (props){
+    return (
+        <div>
+            <img src={props.image} alt=""></img>
+
+        </div>
+    )
+}
---
+++ src/components/Jumbotron/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Jumbotron (){
+    return (
+   
+        <div
+            style={{ height: 560, clear: "both", paddingTop: 120, textAlign: "center" }}
+            className="jumbotron border border-success"
+        > Hey, I'm Ashley
+        </div>
+    );
+}
+
+export default Jumbotron;
