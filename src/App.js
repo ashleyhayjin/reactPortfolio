981ce3aaea2d859ec4031b3afcab6e17539312f8
Ashley Lee <ashleyhayjinlee@gmail.com>
2021-05-31 23:22:51 -0500

    Footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Jumbotron from "./components/Jumbotron";
-
+import Footer from "./components/Footer";
 function App (){
 return(
+    <div>
     <Jumbotron></Jumbotron>
+    <Footer></Footer>
+    </div>
 );
 }
 
--- src/components/Jumbotron/index.js
+++ src/components/Jumbotron/index.js
@@ -2,7 +2,6 @@ import React from "react";
 
 function Jumbotron (){
     return (
-   
         <div
             style={{ height: 560, clear: "both", paddingTop: 120, textAlign: "center" }}
             className="jumbotron border border-success"
